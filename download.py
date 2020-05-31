506d955b97f10de039f2281344e73f7c9832d423
Mario <kdesi@kdesi.sk>
2020-05-31 08:33:45 +0200

    Initial commit

---
+++ download.py
@@ -0,0 +1,18 @@
+import requests
+from clint.textui import progress
+import os
+
+url1 = "https://www.tipos.sk/Documents/Csv/loto1.csv"
+url2 = "https://www.tipos.sk/Documents/Csv/loto2.csv"
+
+def download (url):
+    r = requests.get (url, stream = True)
+    file = os.path.basename (url)
+    with open (file, "wb") as file:
+        total_length = int (r.headers.get ('content-length'))
+        for ch in progress.bar (r.iter_content (chunk_size = 2391975), expected_size = (total_length / 1024) + 1):
+            if ch:
+                file.write (ch)
+
+download (url1)
+download (url2)
