689991212a1450b865e01461ad7e892f06eb5774
Bedna-KU <kdesi@kdesi.sk>
2021-08-10 11:17:54 +0200

    Added file name

--- download.py
+++ download.py
@@ -6,10 +6,9 @@ import os
 url1 = "https://www.tipos.sk/loterie/ciselne-loterie/archiv-vyzrebovanych-cisel?file=loto1"
 url2 = "https://www.tipos.sk/loterie/ciselne-loterie/archiv-vyzrebovanych-cisel?file=loto2"
 
-def download (url):
+def download (url, file):
     r = requests.get (url, stream = True)
     if r.status_code == 200:
-        file = os.path.basename (url)
         with open (file, "wb") as file:
             total_length = int (r.headers.get ('content-length'))
             for ch in progress.bar (r.iter_content (chunk_size = 2391975), expected_size = (total_length / 1024) + 1):
@@ -18,5 +17,5 @@ def download (url):
     else:
         print("Download failed", r.status_code)
 
-download (url1)
-download (url2)
+download (url1, "loto1.csv")
+download (url2, "loto2.csv")
